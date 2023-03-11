276fa2aec38e917d86c77515992303ecd42d0588
PixelMutation <charlie.e.spencer@gmail.com>
2023-03-11 13:26:31 +0000

    add ability to toggle display of msg types

--- Output.py
+++ Output.py
@@ -1,45 +1,55 @@
 # from classes.Comms import Comms
-import colorama
+
 from colorama import Fore
-
-# class prefixType:
-# 	def __init__(self,name,prefixColour,msgColour):
-# 		self.name=name
-# 		self.prefixColour=prefixColour
-# 		self.msgColour=msgColour
-
-PREFIX={
-	"INFO"   :(Fore.GREEN,Fore.WHITE),
-	"WARN"   :(Fore.YELLOW,Fore.WHITE),
-	"ERROR"  :(Fore.RED,Fore.WHITE),
-	"EXCEPT" :(Fore.MAGENTA,Fore.WHITE),
-	"ROVER"    :(Fore.CYAN,Fore.WHITE),
-	"MCU"    :(Fore.BLUE,Fore.WHITE),
-	"PING"	 :(Fore.LIGHTBLACK_EX,Fore.WHITE),
-	"ACK"	 :(Fore.LIGHTMAGENTA_EX,Fore.WHITE),
-	"STATUS" :(Fore.LIGHTGREEN_EX,Fore.WHITE),
-	"RC"	 :(Fore.LIGHTBLUE_EX,Fore.WHITE),
-}
+from enum import Enum
 
 
 
-def colorise(msg:str):
-	for prefix,colour in PREFIX.items():
-		msg=msg.replace(prefix,colour[0]+prefix+colour[1])
-	return msg
+class typeSettings:
+	def __init__(self,colour,show):
+		self.colour=colour
+		self.show=show
+
 # output class for printing to terminal and over TCP
 class Output:
-
 	comms=None
+	msg_types={
+		"TCP"    :typeSettings(Fore.CYAN,True), ## gets replaced by tcp_name 
+		"INFO"   :typeSettings(Fore.GREEN,True),
+		"WARN"   :typeSettings(Fore.YELLOW,True),
+		"ERROR"  :typeSettings(Fore.RED,True),
+		"EXCEPT" :typeSettings(Fore.MAGENTA,True),
+		"MCU"    :typeSettings(Fore.BLUE,True),
+		"PING"	 :typeSettings(Fore.LIGHTBLACK_EX,True),
+		"ACK"	 :typeSettings(Fore.LIGHTMAGENTA_EX,True),
+		"STATUS" :typeSettings(Fore.LIGHTGREEN_EX,True),
+		"RC"	 :typeSettings(Fore.LIGHTBLUE_EX,True),
+	}
+	def __init__(self,tcp_name):
+		self.tcp_name=tcp_name
 	def assignTCP(self,comms):
 		self.comms=comms
 	def write(self,prefix:str,msg:str,tcp=False):
-		# print(msg)
-		if prefix=="TCP":
-			prefix="ROVER"
-		print(colorise(f"{prefix.ljust(6)}: {msg}"))
+		# Only print if enabled for that msg type
+		if prefix in self.msg_types:
+			if self.msg_types[prefix].show==True:
+				print(self.colorise(f"{prefix.ljust(6)}: {msg}"))
+		# Send over TCP if that is enabled for this message
 		if tcp:
 			try:
 				self.comms.send({prefix:msg}) # formatted as a JSON key:val pair
 			except:
 				pass
+
+	def toggleDisplayType(self,type,enabled):
+		if type in self.msgTypes:
+			self.msgTypes[type].show=enabled
+		else:
+			self.write("ERROR",f"Message type {type} not found")
+
+	def colorise(self,msg:str):
+		for type,settings in self.msg_settingss.items():
+			if "TCP" in settings:
+				settings=self.tcp_name
+			msg=msg.replace(type,settings.colour+type+Fore.WHITE)
+		return msg
